bd1e2203810778b2c04776c029ddd6167915b0f4
VitalyOv <vit2013aly@mail.ru>
2022-12-09 20:15:20 +0300

    Add 4 project

--- Common/SystemCoordinate.cs
+++ Common/SystemCoordinate.cs
@@ -1,6 +1,18 @@
 ﻿namespace Common;
 public static class SystemCoordinate
 {
+    public static int GetNumberFromUser()
+    {
+        bool isParsed = int.TryParse(Console.ReadLine(),out int number);
+
+        if(isParsed)
+        {
+            return number;
+        }
+        else{
+            return -1;
+        }
+    }
     public static int GetNumberOfQuarter(int x, int y)
     {
         if (x > 0 && y > 0)
