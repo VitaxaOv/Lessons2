360d67592fb0155996ac9ad511f77a2ce0a27228
VitalyOv <vit2013aly@mail.ru>
2022-12-10 00:47:16 +0300

    Add print array

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,9 +1,8 @@
 ﻿//Задача 24: Напишите программу, которая принимает на вход число (А) 
 //и выдаёт сумму чисел от 1 до А.
 //7 -> 28 4 -> 10 8 -> 36
-using Common;
 
-int a = int.Parse(Console.ReadLine());
+/*int a = int.Parse(Console.ReadLine());
 int sum = GetSumNumbersBetweenOneToNumber(a);
 Console.WriteLine(sum);
 
@@ -16,5 +15,25 @@ int GetSumNumbersBetweenOneToNumber(int number)
     }
 
     return sum;
+}*/
+
+int[] numbers = new int[]{12,3,23};
+PrintArray(numbers);
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write(array[array.Length-1]);
+    Console.Write("]");
 }
+
+Math.Pow(234,2);
+
+int Pow(){
+    
+}
+
 
